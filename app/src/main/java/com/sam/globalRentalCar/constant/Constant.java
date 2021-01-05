3512cbf91a85d4414a6969e26fe19bcbf1215df6
Sam <m15065157316@163.com>
2021-01-05 23:26:45 +0800

    修改用户协议链接

--- app/src/main/java/com/sam/globalRentalCar/constant/Constant.java
+++ app/src/main/java/com/sam/globalRentalCar/constant/Constant.java
@@ -55,7 +55,7 @@ public class Constant {
     /**
      * 个人协议
      */
-    public static final String USER_PROTOTAL = "http://39.106.49.27:28080/userPrototal";
+    public static final String USER_PROTOTAL = "http://39.106.49.27:28080/userPrototal.html";
     /**
      * 租车协议
      */
